a2ae310177789022ef64ccc311a2db539a8b7de6
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 10:54:21 -0700

    import user table in userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,24 @@
 const router = require("express").Router();
 
+//import user table
+const { User } = require('../../models')
+
+router.post('/', async (req, res) => {
+
+
+})
+
+
+router.post("/login", async (req, res) => {});
+
+
+
+
+
+router.post("/logout", async (req, res) => {});
+
+
+
+
 
 module.exports = router;
