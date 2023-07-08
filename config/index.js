778d49aa0731c625b5508bc04df13d093bcf9271
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 16:16:11 -0700

    make it better

--- config/index.js
+++ config/index.js
@@ -2,16 +2,19 @@
 const Sequelize = require("sequelize");
 
 //set up for the sequelize so it knows our mysql password, user and database we created
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+
 
 //export it so it can be used in other file
 module.exports = sequelize;
