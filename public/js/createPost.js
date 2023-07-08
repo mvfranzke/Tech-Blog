b128c99737b234850de96428fd8929c8ed1e12f8
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 15:45:32 -0700

    event listener to save title and body post when use saves it and reload the page to be able to view it and event listener to bring user to homepage when they click on logout button

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,3 +1,4 @@
+//event listener to bring user to homepage when they click on logout button
 document.getElementById("logOutUser").addEventListener("click", async () => {
   await fetch(`/api/users/logout`, {
     method: "POST",
@@ -6,6 +7,7 @@ document.getElementById("logOutUser").addEventListener("click", async () => {
   document.location.replace("/auth");
 });
 
+//event listener to save title and body post when use saves it and reload the page to be able to view it
 document.getElementById("createPost").addEventListener("click", async () => {
   const post = {
     title: document.getElementById("title").value,
