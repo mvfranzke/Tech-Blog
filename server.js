a4b3a0aec4c0cffcd521b2e37ea69959fa02cbe7
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 10:02:05 -0700

    updated port to log in console in server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 //import express package
-const express = require('express')
+const express = require("express");
 
 //declare app variable as express()
-const app = express()
+const app = express();
 
 //setting up port 3001 or env.PORT for running in HEROKU later
 const port = process.env.PORT || 3001;
 
-//set server to listen to port 3001 or log error
+//set server to listen to port
+app.listen(port, () => {
+  console.log(`Listening on PORT: ${port}`);
+});
+
+//sequelize synchronization to occur on the same port and log to console
 sequelize
   .sync({ force: false })
-  .then(() => app.listen (port))
+  .then(() => {
+    console.log(`Sequelize synced successfully`);
+  })
   .catch((err) => console.error(err));
