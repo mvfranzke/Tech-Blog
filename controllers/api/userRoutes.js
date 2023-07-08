996cde9f62ac2086bff3e31061a2bdd38c91eec2
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 11:00:45 -0700

    set up router for user to register, creates and save username and apssword in user table

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,24 +1,40 @@
 const router = require("express").Router();
 
 //import user table
-const { User } = require('../../models')
-
-router.post('/', async (req, res) => {
+const { User } = require('../../models');
 
 
-})
+//registration, sets up new user and password
+router.post("/", async (req, res) => {
+try {
+  const newUser = await User.create({
+    username: req.body.username,
+    password: req.body.password,
+  });
 
+  req.session.save(() => {
+    req.session.userId = newUser.id;
+    req.session.username = newUser.username;
+    req.session.loggedIn = true;
 
+    res.json(newUser);
+  });
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+//logins a user
 router.post("/login", async (req, res) => {});
 
 
 
 
-
+//logs out a user
 router.post("/logout", async (req, res) => {});
 
 
 
 
-
+//export router to be used in other file
 module.exports = router;
