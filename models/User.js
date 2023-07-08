7dae53d1bf2b14015d62450c91ab2eb0a9389bdd
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 10:12:11 -0700

    added hooks for before password gets created it will be encypted first, specify other properties like sequelize, freezetable, timestamp and set modelName

--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const bcrypt = require('bcrypt')
 //import our database connection by grabbing the config folder, this is where we set up mysql password, user name and secret
 const sequelize = require('../config')
 
+//set up class user to extend model properties
 class User extends Model {
   //function to check and validate that we enter the valid password vs. the password stored
   checkPassword(password) {
@@ -16,24 +17,40 @@ class User extends Model {
 
 
 //setting up out table, with the following fields - id, username and password
-User.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //we're going to validate that what they type must be atleat 6 character
+      validate: {
+        len: [6],
+      },
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    //we're going to validate that what they type must be atleat 6 character
-    validate: {
-      len: [4]
-    }
+  //set up hooks for when a password gets created, it will be encrypted first before getting added on the databse
+  {
+    hooks: {
+      beforeCreate: async (data) => {
+        data.password = await bcrypt.hash(data.password, 10);
+        return data;
+      },
+    },
+    sequelize, //database connection, provides method for interacting with the database
+    freezeTableName: true, // keeps our modelName user vs. users
+    timestamps: false, // disables automatic timestamp when table gets updated
+    modelName: "user", // table name in database
   }
-},
+);
 
+module.exports = User; // export class user to be used in other file
