331beaf1d3a4939d45ffb480b72aec1564ddb3c5
mvfranzke <michellevonfranzke@gmail.com>
2023-07-07 22:05:04 -0700

    set up connection for postRoutes and userRoutes in controllers>api>index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,15 @@
+//bring in our router
+const router = require("express").Router();
+
+//import both files, we're connecting
+const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes");
+
+//were going to tell the router to use if it got /users in it, send it to userRoutes
+router.use("/users", userRoutes);
+
+//were going to tell the router to use if it got /post in it, send it to postRoutes
+router.use("/posts", postRoutes);
+
+//exports the router
+module.exports = router;
