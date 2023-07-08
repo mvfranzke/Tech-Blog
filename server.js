62f639753c1b43da073da91b862587d78e5abadf
mvfranzke <michellevonfranzke@gmail.com>
2023-07-07 17:29:20 -0700

    added .env file and assigned port in server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const express = require('express')
 //declare app variable as express()
 const app = express()
 
+//setting up port 3001 or env.PORT for running in HEROKU later
+const port = process.env.PORT || 3001;
+
 //set server to listen to port 3001 or log error
 sequelize
   .sync({ force: false })
-  .then(() => app.listen(3001))
+  .then(() => app.listen (port))
   .catch((err) => console.error(err));
