5172d6cdb1d017e61179894331044cca799cec9e
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 10:06:11 -0700

    set up class user to extend model properties, set user table with the following fields - id, username and password

--- models/User.js
+++ models/User.js
@@ -7,3 +7,33 @@ const bcrypt = require('bcrypt')
 //import our database connection by grabbing the config folder, this is where we set up mysql password, user name and secret
 const sequelize = require('../config')
 
+class User extends Model {
+  //function to check and validate that we enter the valid password vs. the password stored
+  checkPassword(password) {
+    return bcrypt.compareSync(password, this.password);
+  }
+}
+
+
+//setting up out table, with the following fields - id, username and password
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    //we're going to validate that what they type must be atleat 6 character
+    validate: {
+      len: [4]
+    }
+  }
+},
+
