b0f2f3b39df36cfeb052dce9059d21e3f643279e
mvfranzke <michellevonfranzke@gmail.com>
2023-07-08 10:52:04 -0700

    updated notes in postRoutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,8 +1,9 @@
-//import express router, post table and 
+//import express router, post table and withAuth file in utils
 const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+//add post on the application
 router.post("/", withAuth, async (req, res) => {
   try {
     const post = await Post.create({ ...req.body, userId: req.session.userId });
@@ -12,8 +13,7 @@ router.post("/", withAuth, async (req, res) => {
   }
 });
 
-
-
+//edit post on the application
 router.put("/:id", withAuth, async (req, res) => {
   try {
   const [affectedRows] = await Post.update(req.body, {
@@ -32,7 +32,7 @@ router.put("/:id", withAuth, async (req, res) => {
 }});
 
 
-
+//delete post on the application
 router.delete("/:id", withAuth, async (req, res) => {
   try {
     const [affectedRows] = await Post.destroy({
@@ -44,7 +44,7 @@ router.delete("/:id", withAuth, async (req, res) => {
     if (affectedRows > 0) {
       res.status(200).end();
     } else {
-      res.status(404).end();
+      res.status(410).end();
     }
   } catch (err) {
     res.status(500).json(err);
@@ -52,5 +52,5 @@ router.delete("/:id", withAuth, async (req, res) => {
 });
 
 
-
+//export router to be used in other file
 module.exports = router;
